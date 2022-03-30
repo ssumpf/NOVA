ssumpf/NOVA

commit 4ab40e44dc239aae41d4bba6c50572029d825bf7

Flush IOMMU caches on memory revoke

include/iommu_intel.hpp
@@ -34,24 +34,30 @@ class Dmar_qi
 
     public:
         Dmar_qi (uint64 l = 0, uint64 h = 0) : lo (l), hi (h) {}
+
+        enum Mode
+        {
+            FLUSH_GLOBAL = 0x1,
+            FLUSH_BY_DID = 0x2,
+        };
 };
 
 class Dmar_qi_ctx : public Dmar_qi
 {
     public:
-        Dmar_qi_ctx() : Dmar_qi (0x1 | 1UL << 4) {}
+        Dmar_qi_ctx(Mode mode, uint64 did) : Dmar_qi (0x1ULL | (uint64(mode) << 4) | ((did & 0xffffULL) << 16)) {}
 };
 
 class Dmar_qi_tlb : public Dmar_qi
 {
     public:
-        Dmar_qi_tlb() : Dmar_qi (0x2 | 1UL << 4) {}
+        Dmar_qi_tlb(Mode mode, uint64 did) : Dmar_qi (0x22LL | (uint64(mode) << 4) | ((did & 0xffffULL) << 16)) {}
 };
 
 class Dmar_qi_iec : public Dmar_qi
 {
     public:
-        Dmar_qi_iec() : Dmar_qi (0x4 | 1UL << 4) {}
+        Dmar_qi_iec() : Dmar_qi (0x4ULL | 1UL << 4) {}
 };
 
 class Dmar_ctx
@@ -220,17 +226,17 @@ class Dmar : public Iommu::Interface, public List<Dmar>
         }
 
         ALWAYS_INLINE
-        inline void flush_ctx()
+        inline void flush_ctx(Dmar_qi::Mode const mode = Dmar_qi::Mode::FLUSH_GLOBAL, uint64 const domain_id = 0)
         {
             if (qi()) {
-                qi_submit (Dmar_qi_ctx());
-                qi_submit (Dmar_qi_tlb());
+                qi_submit (Dmar_qi_ctx(mode, domain_id));
+                qi_submit (Dmar_qi_tlb(mode, domain_id));
                 qi_wait();
             } else {
-                write<uint64>(REG_CCMD, 1ULL << 63 | 1ULL << 61);
+                write<uint64>(REG_CCMD, (1ULL << 63) | (uint64(mode) << 61) | (domain_id & 0xffffULL));
                 while (read<uint64>(REG_CCMD) & (1ULL << 63))
                     pause();
-                write<uint64>(REG_IOTLB, 1ULL << 63 | 1ULL << 60);
+                write<uint64>(REG_IOTLB, (1ULL << 63) | (uint64(mode) << 60) | ((domain_id & 0xffffULL) << 32));
                 while (read<uint64>(REG_IOTLB) & (1ULL << 63))
                     pause();
             }
@@ -272,6 +278,8 @@ class Dmar : public Iommu::Interface, public List<Dmar>
 
         void assign (uint16, Pd *) override;
         static void release (uint16, Pd *);
+
+        static void flush_pgt(uint16, Pd &);
 
         static void vector (unsigned);
 

src/iommu.cpp
@@ -57,6 +57,9 @@ void Iommu::Interface::release (uint16 const rid, Pd * const pd)
 
 void Iommu::Interface::flush_pgt(uint16 const rid, Pd &pd)
 {
+    if (Dmar::online())
+        Dmar::flush_pgt(rid, pd);
+
     if (Iommu::Amd::online())
         Iommu::Amd::flush_pgt(rid, pd);
 }

src/iommu_intel.cpp
@@ -22,6 +22,7 @@
 #include "bits.hpp"
 #include "iommu_intel.hpp"
 #include "lapic.hpp"
+#include "pci.hpp"
 #include "pd.hpp"
 #include "stdio.hpp"
 #include "vectors.hpp"
@@ -186,4 +187,21 @@ void Dmar::vector (unsigned vector)
     if (EXPECT_TRUE (msi == 0))
         for (Dmar *dmar = list; dmar; dmar = dmar->next)
             if (!dmar->invalid()) dmar->fault_handler();
+}
+
+static Dmar * lookup (uint16 const rid)
+{
+    auto * const obj = Pci::find_iommu (rid);
+    Dmar * iommu = static_cast<Dmar *>(obj);
+    return iommu;
+}
+
+void Dmar::flush_pgt (uint16 const rid, Pd &p)
+{
+    auto iommu = lookup(rid);
+    if (!iommu) return;
+
+    Lock_guard <Spinlock> guard (iommu->lock);
+
+    iommu->flush_ctx(Dmar_qi::Mode::FLUSH_BY_DID, p.dom_id);
 }

commit de90690dbf395a786d10c22123f37feb143a109f

Fix IOTLB flush for global mode
The typo was introduced in

  4ab40e4 Flush IOMMU caches on memory revoke

and always overwrote bit 2 of the G field (granularity). With
global-granularity mode (01b) specified, the result was
Page-Selective-within-Domain Invalidation (11b), which is interpreted as
an invalid descriptor on hardware lacking this feature. The error was
apparent in REG_FSTS bit 4, which is not checked by the current
implementation and results in an endless (polling) wait for termination
of the queued invalidation.

Fixes #6

include/iommu_intel.hpp
@@ -51,7 +51,7 @@ class Dmar_qi_ctx : public Dmar_qi
 class Dmar_qi_tlb : public Dmar_qi
 {
     public:
-        Dmar_qi_tlb(Mode mode, uint64 did) : Dmar_qi (0x22LL | (uint64(mode) << 4) | ((did & 0xffffULL) << 16)) {}
+        Dmar_qi_tlb(Mode mode, uint64 did) : Dmar_qi (0x2ULL | (uint64(mode) << 4) | ((did & 0xffffULL) << 16)) {}
 };
 
 class Dmar_qi_iec : public Dmar_qi